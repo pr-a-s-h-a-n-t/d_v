ba9c6527b478b0d84089ecc789357d5af7485ffc
Prashant Mishra <prashant.mishra@leadtorev.comm>
2023-07-16 06:13:36 +0530

    table update

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,260 +1,260 @@
 import React from "react";
 import TableComponent from "./common/Table/TableComponent";
 import { debounce } from "lodash";
-// import ApiData from "../constants/ApiData";
-export const ApiData = [
-  {
-    "EMPLOYEE ID": 1,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 2,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 3,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 4,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-  {
-    "EMPLOYEE ID": 5,
-    "EMPLOYEE NAME": "Ram",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 6,
-    "EMPLOYEE NAME": "Kishor",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 7,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 8,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 9,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 10,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
+import { ApiData } from "../constants/ApiData";
+// export const ApiData = [
+//   {
+//     "EMPLOYEE ID": 1,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 2,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 3,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 4,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+//   {
+//     "EMPLOYEE ID": 5,
+//     "EMPLOYEE NAME": "Ram",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 6,
+//     "EMPLOYEE NAME": "Kishor",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 7,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 8,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 9,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 10,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
 
-  {
-    "EMPLOYEE ID": 11,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 12,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 13,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 14,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-  {
-    "EMPLOYEE ID": 15,
-    "EMPLOYEE NAME": "Ram",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 16,
-    "EMPLOYEE NAME": "Kishor",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 17,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 18,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 19,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 20,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-  {
-    "EMPLOYEE ID": 21,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 22,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 23,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 24,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-  {
-    "EMPLOYEE ID": 25,
-    "EMPLOYEE NAME": "Ram",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 26,
-    "EMPLOYEE NAME": "Kishor",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 27,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 28,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 29,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 30,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-  {
-    "EMPLOYEE ID": 31,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 32,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 33,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 34,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-  {
-    "EMPLOYEE ID": 35,
-    "EMPLOYEE NAME": "Ram",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 36,
-    "EMPLOYEE NAME": "Kishor",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 37,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 38,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 39,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 40,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-  {
-    "EMPLOYEE ID": 41,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 42,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 43,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 44,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-  {
-    "EMPLOYEE ID": 45,
-    "EMPLOYEE NAME": "Ram",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 46,
-    "EMPLOYEE NAME": "Kishor",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 47,
-    "EMPLOYEE NAME": "Raj",
-    "EMPLOYEE DESCRIPTION": ".NET Developer",
-  },
-  {
-    "EMPLOYEE ID": 48,
-    "EMPLOYEE NAME": "Mano",
-    "EMPLOYEE DESCRIPTION": "Angular Developer",
-  },
-  {
-    "EMPLOYEE ID": 49,
-    "EMPLOYEE NAME": "Mohan",
-    "EMPLOYEE DESCRIPTION": "React Developer",
-  },
-  {
-    "EMPLOYEE ID": 50,
-    "EMPLOYEE NAME": "Hari",
-    "EMPLOYEE DESCRIPTION": "Design Engineer",
-  },
-];
+//   {
+//     "EMPLOYEE ID": 11,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 12,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 13,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 14,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+//   {
+//     "EMPLOYEE ID": 15,
+//     "EMPLOYEE NAME": "Ram",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 16,
+//     "EMPLOYEE NAME": "Kishor",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 17,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 18,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 19,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 20,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+//   {
+//     "EMPLOYEE ID": 21,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 22,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 23,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 24,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+//   {
+//     "EMPLOYEE ID": 25,
+//     "EMPLOYEE NAME": "Ram",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 26,
+//     "EMPLOYEE NAME": "Kishor",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 27,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 28,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 29,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 30,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+//   {
+//     "EMPLOYEE ID": 31,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 32,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 33,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 34,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+//   {
+//     "EMPLOYEE ID": 35,
+//     "EMPLOYEE NAME": "Ram",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 36,
+//     "EMPLOYEE NAME": "Kishor",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 37,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 38,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 39,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 40,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+//   {
+//     "EMPLOYEE ID": 41,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 42,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 43,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 44,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+//   {
+//     "EMPLOYEE ID": 45,
+//     "EMPLOYEE NAME": "Ram",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 46,
+//     "EMPLOYEE NAME": "Kishor",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 47,
+//     "EMPLOYEE NAME": "Raj",
+//     "EMPLOYEE DESCRIPTION": ".NET Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 48,
+//     "EMPLOYEE NAME": "Mano",
+//     "EMPLOYEE DESCRIPTION": "Angular Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 49,
+//     "EMPLOYEE NAME": "Mohan",
+//     "EMPLOYEE DESCRIPTION": "React Developer",
+//   },
+//   {
+//     "EMPLOYEE ID": 50,
+//     "EMPLOYEE NAME": "Hari",
+//     "EMPLOYEE DESCRIPTION": "Design Engineer",
+//   },
+// ];
 
 function Table() {
   const [searchValue, setSearchValue] = React.useState("");
@@ -279,12 +279,20 @@ function Table() {
   }, [searchValue]);
 
   return (
-    <div>
+    <div
+      style={{
+        width: "100%",
+      }}
+    >
       <div>
         <input type="text" onChange={(e) => handleSearch(e.target.value)} />
       </div>
-      <div>
-        <TableComponent data={filteredData} /> {/* Pass the filtered data */}
+      <div
+        style={{
+          width: "100%",
+        }}
+      >
+        <TableComponent data={ApiData} /> {/* Pass the filtered data */}
       </div>
     </div>
   );
--- src/components/common/Table/TableComponent.jsx
+++ src/components/common/Table/TableComponent.jsx
@@ -1,62 +1,116 @@
-import React, { useState } from "react";
+import React from "react";
+import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
-const itemsPerPage = 10;
+const columns = [
+  { id: "EMPLOYEE ID", label: "Employee ID", minWidth: 100 },
+  { id: "EMPLOYEE NAME", label: "Employee Name", minWidth: 170 },
+  { id: "EMPLOYEE DESCRIPTION", label: "Employee Description", minWidth: 200 },
+  { id: "DELETE BUTTON", label: "Delete Button", minWidth: 100 },
+];
 
-export default function BasicTable({ ApiData }) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [allRows] = useState(ApiData);
-  const pageCount = Math.ceil(allRows.length / itemsPerPage);
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = Math.min(startIndex + itemsPerPage, allRows.length);
-  const rows = allRows.slice(startIndex, endIndex);
+function createData(id, name, description) {
+  return { id, name, description };
+}
 
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
+const rows = [
+  createData(1, "Raj", ".NET Developer"),
+  createData(2, "Mano", "Angular Developer"),
+  createData(3, "Mohan", "React Developer"),
+  // Add more data rows here
+];
+
+export default function StickyHeadTable({ data }) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(50);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleDelete = (id) => {
+    // Perform the delete operation based on the id
+    console.log("Delete entry with ID:", id);
   };
 
   return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer
+        sx={{
+          width: "100%",
+          maxHeight: "500px",
+          overflow: "scroll",
+        }}
+      >
+        <Table sx={{ width: "100%" }} stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-              {Object.keys(rows[0]).map((header) => (
-                <TableCell key={header}>{header}</TableCell>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row, index) => (
-              <TableRow
-                key={index}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                {Object.values(row).map((value, index) => (
-                  <TableCell key={index}>{value}</TableCell>
-                ))}
-              </TableRow>
-            ))}
+            {data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                    {columns.map((column) => {
+                      if (column.id === "DELETE BUTTON") {
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            <Button
+                              variant="contained"
+                              color="secondary"
+                              onClick={() => handleDelete(row.id)}
+                            >
+                              Delete
+                            </Button>
+                          </TableCell>
+                        );
+                      }
+
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
           </TableBody>
         </Table>
       </TableContainer>
-      <div>
-        {Array.from({ length: pageCount }, (_, index) => (
-          <button
-            key={index}
-            onClick={() => handlePageChange(index + 1)}
-            disabled={currentPage === index + 1}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
-    </>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 }
